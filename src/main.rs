a67d44d7ee918668a574cfb68f714d6830d9dff6
RustyRobot <rustfmtbot@yandex.ru>
2018-12-03 00:28:43 +0300

    rustyrobot formatting

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
-
 extern crate serde;
 extern crate serde_json;
 
 #[macro_use]
 extern crate serde_derive;
 
-mod instance;
 mod config;
+mod instance;
 
 fn main() {
-
-	let inst = instance::Instance::new();
-	match inst {
-		Ok(inst) => inst.handle(),
-		Err(err) => match err {
-			instance::InstanceErrors::CouldNotFindHomeDir => println!("could not find home directory."),
-			instance::InstanceErrors::CouldNotParseConfig => println!("could not parse configuration.")
-		}
-	}
+    let inst = instance::Instance::new();
+    match inst {
+        Ok(inst) => inst.handle(),
+        Err(err) => match err {
+            instance::InstanceErrors::CouldNotFindHomeDir => {
+                println!("could not find home directory.")
+            }
+            instance::InstanceErrors::CouldNotParseConfig => {
+                println!("could not parse configuration.")
+            }
+        },
+    }
 }
